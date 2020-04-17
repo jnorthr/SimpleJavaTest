0484cafa00baa76488c84b8c84d15b6dd284295d
jnorthr <james.b.northrop@googlemail.com>
2020-04-17 18:16:44 +0200

    first try at simple Java with Gradle

---
+++ src/main/java/com/jnorthr/Simple.java
@@ -0,0 +1,10 @@
+// File: src/main/java/com/jnorthr/Simple.java
+package com.jnorthr;
+ 
+public class Simple {
+ 
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("simple.message") + args[0] + " from Simple.");
+    }
+ 
+} // end of simple
